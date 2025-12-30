5f49cd8ff5c26c911d5a3c74bde0824bd95d8682
glagol05 <vehiddemic@gmail.com>
2025-12-30 13:03:20 +0800

    added list of adjectives

---
+++ adjective-base/irregular.js
@@ -0,0 +1,24 @@
+export const irregularAdjectives = [
+  { base: "bonus", fem: "bona", neut: "bonum", comparative: "melior", superlative: "optimus" },
+  { base: "malus", fem: "mala", neut: "malum", comparative: "peior", superlative: "pessimus" },
+  { base: "magnus", fem: "magna", neut: "magnum", comparative: "maior", superlative: "maximus" },
+  { base: "parvus", fem: "parva", neut: "parvum", comparative: "minor", superlative: "minimus" },
+  { base: "multus", fem: "multa", neut: "multum", comparative: "plus", superlative: "plurimus" },
+  { base: "superus", fem: "supera", neut: "superum", comparative: "superior", superlative: "summus" },
+  { base: "inferus", fem: "infera", neut: "inferum", comparative: "inferior", superlative: "infimus" },
+  { base: "prius", fem: "prior", neut: "prior", comparative: "prior", superlative: "primus" },
+  { base: "posterus", fem: "postera", neut: "posterum", comparative: "posterior", superlative: "postumus" },
+  { base: "solus", fem: "sola", neut: "solum", comparative: "solior", superlative: "solissimus" },
+  { base: "ullus", fem: "ulla", neut: "ullum", comparative: "ullior", superlative: "ullissimus" },
+  { base: "neuter", fem: "neutra", neut: "neutrum", comparative: "neutrior", superlative: "neutrissimus" },
+  { base: "alter", fem: "altera", neut: "alterum", comparative: "alterior", superlative: "alterrimus" },
+  { base: "uter", fem: "utra", neut: "utrum", comparative: "utrior", superlative: "utrissimus" },
+  { base: "nullus", fem: "nulla", neut: "nullum", comparative: "nullior", superlative: "nullissimus" },
+  { base: "totus", fem: "tota", neut: "totum", comparative: "tottior", superlative: "tottissimus" },
+  { base: "alter", fem: "altera", neut: "alterum", comparative: "alterior", superlative: "alterrimus" },
+  { base: "neuter", fem: "neutra", neut: "neutrum", comparative: "neutrior", superlative: "neutrissimus" },
+  { base: "prior", fem: "prior", neut: "prior", comparative: "prior", superlative: "primus" },
+  { base: "posterior", fem: "posterior", neut: "posterior", comparative: "posterior", superlative: "postumus" }
+].map(lemma => {
+    
+})
